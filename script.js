e2c682790e54d7f21c35f7085b741872bc0400c2
Ernesto Espinoza M <emenocal07@gmail.com>
2021-12-30 20:43:46 +0100

    Start app

---
+++ script.js
@@ -0,0 +1,14 @@
+// document.getElementById("")
+// document.querySelector("")
+
+//Total Balance
+const balanceElement = document.getElementById("balance");
+console.log(balancedElement);
+
+//Value input
+
+//Income/expense
+
+//Addition button
+
+//Entry list
