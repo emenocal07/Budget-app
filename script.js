a1db8e50e44bef47882a6bdabf8e99865ef2de15
Ernesto Espinoza M <emenocal07@gmail.com>
2022-01-02 19:02:05 +0100

    Finish js

--- script.js
+++ script.js
@@ -1,37 +1,73 @@
-// document.getElementById("")
-// document.querySelector("")
+// document.getElementById('')
+// document.querySelector('')
 
-//Total Balance
+// Total Balance
 const balanceElement = document.getElementById("balance");
-console.log(balancedElement);
 
-//Value input
+// Value input
 const valueInputElement = document.querySelector("input");
-//Income/expense
+
+// Income/expense
 const selectElement = document.querySelector("select");
-//Addition button
+
+// Addition button
 const additionButtonElement = document.querySelector("footer button");
-//Entry list
+
+// Entry list
 const entryListElement = document.querySelector("section ul");
 
-function addEntry (amount, operation) {
-    const listEntry = document.createElement("li");
+function addEntry(amount, operation) {
+  const listEntry = document.createElement("li");
+  listEntry.classList.add(operation);
 
-    const listEntryValue = document.createElement("strong");
-    listEntryValue.innerText = amount;
+  const listEntryValue = document.createElement("strong");
+  listEntryValue.innerText = amount + "$";
 
-    const listEntryDescription = document.createElement("em");
-    listEntryDescription.innerText = "Description";
+  const listEntryDescription = document.createElement("em");
+  listEntryDescription.innerText = "Description";
 
-    listEntry.appendChild(listEntryDescription);
-    listEntry.appendChild(listEntryValue);
-    
-    entryListElement.appendChild(listEntry);
+  const listEntryOperator = document.createElement("span");
+
+  if (operation === "income") {
+    listEntryOperator.innerText = "+";
+  } else if (operation === "expense") {
+    listEntryOperator.innerText = "-";
+  }
+
+  listEntry.appendChild(listEntryDescription);
+  listEntry.appendChild(listEntryOperator);
+  listEntry.appendChild(listEntryValue);
+
+  entryListElement.appendChild(listEntry);
+}
+
+function updateBalance() {
+  let total = 0;
+
+  for (let item of entryListElement.children) {
+    const valueElement = item.querySelector("strong");
+    const operationElement = item.querySelector("span");
+
+    const value = parseInt(valueElement.innerText);
+    const operation = operationElement.innerText;
+
+    if (operation === "+") {
+      total = total + value;
+    } else if (operation === "-") {
+      total = total - value;
+    }
+  }
+
+  balanceElement.innerText = total + "$";
 }
 
 additionButtonElement.onclick = function() {
-    const amount = valueInputElement.value;
-    const operation = selectElement.value;
+  const amount = valueInputElement.value;
+  const operation = selectElement.value;
 
-    addEntry(amount, operation);
-}
+  addEntry(amount, operation);
+
+  valueInputElement.value = "";
+
+  updateBalance();
+};
